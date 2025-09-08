9577a67773f91428977ab24c48ddf4fdf1d9e5c3
taiseen <taiseen.cse@gmail.com>
2025-09-08 23:11:12 +0600

    feat: Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
 
 export default function Home() {
-  return <div className="text-red-500">Hello</div>;
+  return (
+    <main>
+      <Hero />
+    </main>
+  );
 }
